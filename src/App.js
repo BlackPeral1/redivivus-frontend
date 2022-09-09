8a79aad06fadc84ee2df4614a55921a4ad729bb2
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2022-09-09 16:12:53 +0530

    Landing page created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Spinner from 'react-bootstrap/Spinner';
+import Landing from './views/common/Landing';
 
 const loading = (
   <Spinner animation="border" role="status">
@@ -20,12 +21,15 @@ const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 function App() {
   return (
     <BrowserRouter>
+  
       <Suspense fallback={loading}>
         <Routes>
+          {/* <Route path="/home" element={<Home/>}/> */}
           {/* <Route exact path="/login" name="Login Page" element={<Login />} />
           <Route exact path="/register" name="Register Page" element={<Register />} />
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} /> */}
+           <Route path="/landing" name="Landing" element={<Landing />} />
           <Route path="*" name="Home" element={<DefaultLayout />} />
         </Routes>
       </Suspense>
