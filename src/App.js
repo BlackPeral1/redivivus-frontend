e3558abf8c99f33d5f0b80b7f90b1f1c9957d296
chamathjayasekara99 <it20654962@my.sliit.lk>
2022-09-15 08:58:15 +0530

    App.js file changed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, { Component, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Login from './views/common/login/LoginPage'
-import Spinner from 'react-bootstrap/Spinner';
-import Landing from './views/common/landing/Landing';
-import RegistrationIntro from './views/common/registration-intro/RegistrationIntro';
-
 import Spinner from 'react-bootstrap/Spinner'
+import Landing from './views/common/landing/Landing'
+import RegistrationIntro from './views/common/registration-intro/RegistrationIntro'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-
 
 const loading = (
   <Spinner animation="border" role="status">
@@ -29,18 +26,16 @@ const ContactUsPage = React.lazy(() => import('./components/contact-us/ContactUs
 function App() {
   return (
     <BrowserRouter>
-  
       <Suspense fallback={loading}>
         <Routes>
-
-        <Route path="/login" name="Login" element={<Login />} />
-        <Route path="/registration-intro" name="Reg-intro" element={<RegistrationIntro />} />
+          <Route path="/login" name="Login" element={<Login />} />
+          <Route path="/registration-intro" name="Reg-intro" element={<RegistrationIntro />} />
           {/* <Route path="/home" element={<Home/>}/> */}
           {/* <Route exact path="/login" name="Login Page" element={<Login />} />
           <Route exact path="/register" name="Register Page" element={<Register />} />
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} /> */}
-           <Route path="/landing" name="Landing" element={<Landing />} />
+          <Route path="/landing" name="Landing" element={<Landing />} />
           <Route path="*" name="Home" element={<DefaultLayout />} />
           <Route path="/contact-us" name="contact-us" element={<ContactUsPage />} />
         </Routes>
