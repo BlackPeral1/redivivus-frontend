15c59762bd2befce5d9f095352fe6581c2d52f55
chamathajayasekara99 <it20654962@my.sliit.lk>
2022-09-22 07:55:19 +0530

    Company payment view front end ui design finished

--- src/views/company-payment/make-payment/MakePayment.jsx
+++ src/views/company-payment/make-payment/MakePayment.jsx
@@ -1,9 +1,187 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Multiselect from 'multiselect-react-dropdown'
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const MakePayment = () => {
+  const [validated, setValidated] = useState(false)
+  const [location, setLocation] = useState({})
+  const [form, setForm] = useState({})
+  // const [error, setError] = useState({})
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const inForm = e.currentTarget
+    if (inForm.checkValidity() === false) {
+      setValidated(true)
+    } else {
+      axios
+        .post('http://localhost:3001/api/pickupRequest', form)
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error)
+        })
+        .then(function () {
+          // always executed
+        })
+      Swal.fire({
+        icon: 'success',
+        title: 'Request successfully sent!',
+        showConfirmButton: false,
+        timer: 2000,
+      })
+      setForm({
+        company: 'Select Company',
+        address: '',
+        size: 'Select Size',
+      })
+      setValidated(false)
+    }
+  }
+
+  useEffect(() => {
+    if (navigator?.geolocation) {
+      navigator.geolocation.getCurrentPosition((location) => {
+        if (location) selectLocation(location.coords)
+      })
+    }
+  }, [])
+
+  const selectLocation = (location) => {
+    setLocation({
+      ...form,
+      location: location,
+    })
+  }
+
+  const handleSelectChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    })
+  }
+  const changeWasteTypes = (value) => {
+    setForm({
+      ...form,
+      wasteTypes: value,
+    })
+  }
+  const removeWasteTypes = (value) => {
+    setForm({
+      ...form,
+      wasteTypes: value,
+    })
+  }
   return (
     <>
-      <h1>Make Payment</h1>
+      <h4 className="content-title mt-5">Make Payment</h4>
+      <hr />
+      <div className="row">
+        <div className="card p-4 form">
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row className="mb-4">
+              <Form.Group as={Col} md="6" controlId="validationCustom01" className="d-flex">
+                <Col md="4">
+                  <Form.Label>Payment Type :</Form.Label>
+                </Col>
+                <Col md="8">
+                  <Form.Select value={form.paymentType} name="paymentType">
+                    <option>Visa</option>
+                    <option>Master Card</option>
+                    <option>Credit Card</option>
+                  </Form.Select>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Col} md="6" controlId="validationCustom01" className="d-flex">
+                <Col md="4">
+                  <Form.Label>Card. No. : </Form.Label>
+                </Col>
+                <Col md="8">
+                  <Form.Select value={form.cardNo} name="cardNo">
+                    <option>1089 0067 0005 6000</option>
+                    <option>1089 0067 0005 6000</option>
+                    <option>1089 0067 0005 6000</option>
+                    <option>1089 0067 0005 6000</option>
+                  </Form.Select>
+                </Col>
+              </Form.Group>
+            </Row>
+            <Row className="mb-4">
+              <Form.Group as={Col} md="6" controlId="validationCustom01" className="d-flex">
+                <Col md="4">
+                  <Form.Label>Request ID :</Form.Label>
+                </Col>
+                <Col md="8">
+                  <Form.Select value={form.requestId} name="requestId">
+                    <option>REQID10890067</option>
+                    <option>REQID10890067</option>
+                    <option>REQID10890067</option>
+                  </Form.Select>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Col} md="6" controlId="validationCustom01" className="d-flex">
+                <Col md="4">
+                  <Form.Label>Transaction Date :</Form.Label>
+                </Col>
+                <Col md="8">
+                  <Form.Control type="date" value={form.date} name="date" />
+                </Col>
+              </Form.Group>
+            </Row>
+            <Row className="mb-4">
+              <Form.Group as={Col} md="6" controlId="validationCustom01" className="d-flex">
+                <Col md="4">
+                  <Form.Label>Amount :</Form.Label>
+                </Col>
+                <Col md="8">
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Amount"
+                    onChange={handleSelectChange}
+                    value={form.amount}
+                    name="amount"
+                  />
+                </Col>
+              </Form.Group>
+            </Row>
+            <Row className="mb-4">
+              <Form.Group as={Col} md="12" controlId="validationCustom01" className="d-flex">
+                <Col md="12">
+                  <Form.Control
+                    as="textarea"
+                    rows={5}
+                    style={{ resize: 'none' }}
+                    placeholder="Leave a note"
+                  />
+                </Col>
+              </Form.Group>
+            </Row>
+            <Col md="12" className="d-flex justify-content-end">
+              {/* <Button>Reset</Button> */}
+              <Button
+                type="reset"
+                style={{ backgroundColor: '#36ECAF', color: '#4F4E4E', marginRight: '5%' }}
+              >
+                Clear
+              </Button>
+              <Button
+                type="submit"
+                style={{ backgroundColor: '#36ECAF', color: '#4F4E4E', marginRight: '1.25%' }}
+              >
+                Add
+              </Button>
+            </Col>
+          </Form>
+        </div>
+      </div>
     </>
   )
 }
--- src/views/company-payment/view-company-payments/ViewCompanyPayments.jsx
+++ src/views/company-payment/view-company-payments/ViewCompanyPayments.jsx
@@ -1,10 +1,166 @@
-import React, { useState } from 'react'
+import DataTable from 'react-data-table-component'
+import dumyRequestPayments from '../../../data/dumyData'
+import { useNavigate } from 'react-router-dom'
+import BinRequestServices from '../../../services/BinRequestServices'
+import React, { useState, useEffect } from 'react'
+import readMore from '../../../assets/images/table-icon/read-more.png'
+// Blatant "inspiration" from https://codepen.io/Jacqueline34/pen/pyVoWr
+function convertArrayOfObjectsToCSV(array) {
+  let result
 
+  const columnDelimiter = ','
+  const lineDelimiter = '\n'
+  const keys = Object.keys(dumyRequestPayments[0])
+
+  result = ''
+  result += keys.join(columnDelimiter)
+  result += lineDelimiter
+
+  array.forEach((item) => {
+    let ctr = 0
+    keys.forEach((key) => {
+      if (ctr > 0) result += columnDelimiter
+
+      result += item[key]
+
+      ctr++
+    })
+    result += lineDelimiter
+  })
+
+  return result
+}
+const Export = ({ onExport }) => (
+  <button className="btn btn-secondary " onClick={(e) => onExport(e.target.value)}>
+    <i className="fal fa-file-download"></i>
+    Generate Report
+  </button>
+)
+// Blatant "inspiration" from https://codepen.io/Jacqueline34/pen/pyVoWr
+function downloadCSV(array) {
+  const link = document.createElement('a')
+  let csv = convertArrayOfObjectsToCSV(array)
+  if (csv == null) return
+
+  const filename = 'export.csv'
+
+  if (!csv.match(/^data:text\/csv/i)) {
+    csv = `data:text/csv;charset=utf-8,${csv}`
+  }
+
+  link.setAttribute('href', encodeURI(csv))
+  link.setAttribute('download', filename)
+  link.click()
+}
 const ViewCompanyPayments = () => {
+  const navigate = useNavigate()
+
+  const [search, setSearch] = useState('')
+  const [paymentIdS, setPaymentIds] = useState([])
+  const [data, setData] = useState([])
+  const [filteredData, setFilteredData] = useState([])
+  const actionsMemo = React.useMemo(() => <Export onExport={() => downloadCSV(data)} />, [])
+
+  const viewMore = (requestId) => {
+    navigate(`/admin-company-payments/viewonepayment/${requestId}`)
+  }
+  const columns = [
+    {
+      name: 'PAYMENT ID',
+      selector: (row) => row.payment.paymentId,
+      sortable: true,
+    },
+
+    {
+      name: 'REQUEST ID',
+      selector: (row) => row.requestId,
+      sortable: true,
+    },
+    {
+      name: 'PAID DATE',
+      selector: (row) => row.payment.paidDate,
+      sortable: true,
+    },
+    {
+      name: 'COMPANY PAID',
+      selector: (row) => row.payment.companyPaid,
+      sortable: true,
+    },
+
+    {
+      name: 'DEDUCT AMOUNT',
+      selector: (row) => row.payment.customerEarned,
+      sortable: true,
+    },
+
+    {
+      name: 'STATUS',
+      selector: (row) => row.payment.profit,
+      sortable: true,
+    },
+    {
+      cell: (row) => (
+        <div>
+          <button className="mx-auto btn" onClick={() => viewMore(row.requestId)}>
+            <span className="material-icons">
+              <img src={readMore} alt="" />
+            </span>
+          </button>
+          <button className="mx-auto btn" onClick={() => viewMore(row.requestId)}>
+            <span className="material-icons">
+              <img src={readMore} alt="" />
+            </span>
+          </button>
+        </div>
+      ),
+
+      name: 'ACTION',
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: true,
+    },
+  ]
+  useEffect(() => {}, [])
+  useEffect(() => {
+    const result = filteredData.filter((dataItem) => {
+      if (search === '') {
+        return dataItem
+      } else if (dataItem.requestId.toLowerCase().includes(search.toLowerCase())) {
+        return dataItem
+      }
+    })
+    setData(result)
+  }, [search])
   return (
     <>
-      {' '}
-      <h1>ViewCompanyPayments</h1>
+      <h4 className="content-title mt-5">All Payments </h4>
+      <hr />
+      <div className="main shadow-lg mb-5 rounded-3 mt-5">
+        <DataTable
+          columns={columns}
+          data={data}
+          pagination
+          fixedHeaderScrollHeight="450px"
+          selecttableRowsHighlighted
+          highlightOnHover
+          // onRowClicked={onRowClicked}
+          data-tag="allowRowEvents"
+          subHeader
+          subHeaderComponent={
+            <div className="w-100">
+              {' '}
+              <input
+                type="text"
+                className="w-25 form-control font-color float-center mt-0.5 me-2"
+                placeholder="Search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              ></input>
+            </div>
+          }
+          actions={actionsMemo}
+        />
+      </div>
     </>
   )
 }
