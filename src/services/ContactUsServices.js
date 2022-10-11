c42a934148a2c4b258cf1a3ce96b1ef42a0c160e
Nilupul Madhawa <dnmadawa@gmail.com>
2022-10-11 13:28:05 +0530

    axios config added

--- src/services/ContactUsServices.js
+++ src/services/ContactUsServices.js
@@ -2,9 +2,16 @@ import axios from 'axios'
 
 const QUERY_BASE_URL = 'http://localhost:3001/api/contactus'
 
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.token}`,
+  },
+};
+
 class ContactUsService {
   setQueryDetails(query) {
-    return axios.post(QUERY_BASE_URL, query)
+    return axios.post(QUERY_BASE_URL, query, config)
   }
 }
 
