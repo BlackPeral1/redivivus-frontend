0ddc3d70256fc523073bac6731d7bbdbd3674e1e
Nilupul Madhawa <dnmadawa@gmail.com>
2022-10-13 21:32:29 +0530

    dashbord button

--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -17,21 +17,29 @@ function TopNav() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
 
-          
+
 
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/buyers">Buyers</Nav.Link>
-  <Nav.Link href="#contact-us">Contact</Nav.Link>
+            <Nav.Link href="#contact-us">Contact</Nav.Link>
           </Nav>
-          <Nav>
-            <Nav.Link href="/login">
-              <Button variant="text">Login</Button>
-            </Nav.Link>
-            <Nav.Link eventKey={2} href="/registration-intro">
-              <Button>Signup</Button>
-            </Nav.Link>
-          </Nav>
+
+          {!localStorage.getItem('token') &&
+            <Nav>
+              <Nav.Link href="/login">
+                <Button variant="text">Login</Button>
+              </Nav.Link>
+              <Nav.Link eventKey={2} href="/registration-intro">
+                <Button>Signup</Button>
+              </Nav.Link>
+            </Nav>}
+          {localStorage.getItem('token') &&
+            <Nav>
+              <Nav.Link href="/user">
+                DashBoard
+              </Nav.Link>
+            </Nav>}
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- src/views/common/login/LoginPage.jsx
+++ src/views/common/login/LoginPage.jsx
@@ -9,89 +9,89 @@ import TopNav from "../../../components/topnav/TopNav";
 import { useState } from "react";
 import axios from "axios";
 import Swal from 'sweetalert2'
-
+import { useNavigate } from "react-router-dom";
 export default function Login() {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
-    };
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
-    const [email , setEmail] = useState("")
-    const [password , setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios
-        .post('http://localhost:3001/api/auth/login', {email,password})
-        .then(function (res) {
-          if(res.status === 200){
-            localStorage.setItem("role" , res.data.data.user.role)
-            localStorage.setItem("token" , res.data.data.access_token)
-            Swal.fire({
-                icon: 'success',
-                title: 'Logged in successfully!',
-                showConfirmButton: false,
-                timer: 2000,
-              })
-          }
-        })
-        .catch(function (error) {
-          // handle error
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios
+      .post('http://localhost:3001/api/auth/login', { email, password })
+      .then(function (res) {
+        if (res.status === 200) {
+          localStorage.setItem("role", res.data.data.user.role)
+          localStorage.setItem("token", res.data.data.access_token)
           Swal.fire({
-            icon: 'error',
-            title: 'Failed to login!',
+            icon: 'success',
+            title: 'Logged in successfully!',
             showConfirmButton: false,
             timer: 2000,
           })
-          console.log(error)
+        }
+      })
+      .catch(function (error) {
+        // handle error
+        Swal.fire({
+          icon: 'error',
+          title: 'Failed to login!',
+          showConfirmButton: false,
+          timer: 2000,
         })
-        .then(function () {
-          // always executed
-        })
-    }
+        console.log(error)
+      })
+      .then(function () {
+        // always executed
+      })
+  }
 
-    return (
-        <>
-            <TopNav />
-            <Container className="mt-5" >
-                <Row>
-                    <Col >
-                        <Lottie
-                            options={defaultOptions}
-                            height={300}
-                            width={300} />
-                    </Col>
-                    <Col>
-                        <Form className="mt-5 " onSubmit={handleSubmit}>
-                            <Form.Group className="mb-3 w-75 ms-5" controlId="formBasicEmail">
-                                <Form.Control type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                            </Form.Group>
-                            <Form.Group className="mb-3 w-75 ms-5" controlId="formBasicPassword">
-                                <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                            </Form.Group>
-                            <Form.Group className="text-center">
-                                <Button variant="primary" type="submit" >
-                                    Login
-                                </Button>
-                            </Form.Group>
-                            <Form.Group className="text-center  mt-3" controlId="formBasicCheckbox">
-                                <Form.Text className="text-primary">
-                                    <Nav.Link>
-                                        Not a member? Let's Sign Up!
-                                    </Nav.Link>
-                                </Form.Text>
+  return (
+    <>
+      <TopNav />
+      <Container className="mt-5" >
+        <Row>
+          <Col >
+            <Lottie
+              options={defaultOptions}
+              height={300}
+              width={300} />
+          </Col>
+          <Col>
+            <Form className="mt-5 " onSubmit={handleSubmit}>
+              <Form.Group className="mb-3 w-75 ms-5" controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              </Form.Group>
+              <Form.Group className="mb-3 w-75 ms-5" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </Form.Group>
+              <Form.Group className="text-center">
+                <Button variant="primary" type="submit" >
+                  Login
+                </Button>
+              </Form.Group>
+              <Form.Group className="text-center  mt-3" controlId="formBasicCheckbox">
+                <Form.Text className="text-primary">
+                  <Nav.Link>
+                    Not a member? Let's Sign Up!
+                  </Nav.Link>
+                </Form.Text>
 
-                            </Form.Group>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
 
-        </>
-    );
+    </>
+  );
 
 }
